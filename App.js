ca45bf2e2af9df599fe2ad2016096a543d4729d2
Tejas Patel <tejas7016652830@gmail.com>
2023-09-21 16:26:16 +0530

    Parcel Execution Code Modified

--- App.js
+++ App.js
@@ -1,25 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const perent = React.createElement("div", { id: "perent" }, [
-  React.createElement("div", { id: "cild" }, [
-    React.createElement("h1", {}, "This is heading come frome React"),
-    React.createElement(
-      "h1",
-      {},
-      "This is another h1 which is come frome react"
-    ),
-  ]),
-  React.createElement("div", { id: "cild" }, [
-    React.createElement("h1", {}, "This is heading come frome React"),
-    React.createElement(
-      "h1",
-      {},
-      "This is another h1 which is come frome react"
-    ),
-  ]),
-]);
+//this is one way to write React Element
+const perent = React.createElement(
+  "h1",
+  { className: "head" },
+  "This is namaste React Course"
+);
+
+//Create Element Using JSX
+//JSX--its looks like html and using babel we can compiled into ReactElement
+const head = <h1 className="head">This Is JSX</h1>;
+const head2 = <h1>Head1</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(perent);
+root.render(head);
