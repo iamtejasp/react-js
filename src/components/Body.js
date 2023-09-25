ca62b93776ee7e38d846f28757d7063301550fd2
Tejas Patel <tejas7016652830@gmail.com>
2023-09-25 14:20:33 +0530

    most rated logic added using hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,35 @@
 import Card from "./Card";
 import restList from "../utils/API";
+import { useState } from "react";
 
 const Body = () => {
+  //create variable using hooks ==> using useState
+  const [restaurantList, setRestaurantList] = useState(restList);
+  // console.log(restaurantList);
+  //we can update restaurantList using setRestaurantList Function
+  // filterRest = restaurantList.filter((obj) => obj.data.avgRating > 4);
+  // setRestaurantList(filterRest);
+  // console.log(restaurantList);
+  // normal Js Variable
+  // let restaurantList = restList;
   return (
     <div className="main-container">
-      {/* <div className="search-bar">search</div> */}
+      <div className="search-bar">
+        <button
+          className="filter-card"
+          onClick={() => {
+            const filterRestaurant = restaurantList.filter((res) => {
+              return res.data.avgRating > 4.2;
+            });
+            //using this we can update the rest list
+            setRestaurantList(filterRestaurant);
+          }}
+        >
+          Most Reated
+        </button>
+      </div>
       <div className="cart-container">
-        {restList.map((resObj) => (
+        {restaurantList.map((resObj) => (
           <Card key={resObj.data.id} resData={resObj} />
         ))}
       </div>
