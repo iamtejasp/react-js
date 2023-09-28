4ea44efb79b9e8c7e6b28a65756b4a440126e622
Tejas Patel <tejas7016652830@gmail.com>
2023-09-28 15:24:40 +0530

    Layout Change

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,26 @@
 const Header = () => {
   return (
-    <div className="nav">
-      <div className="logo-container">
-        <img src={require("../assest/logo.png")} alt="#" />
-      </div>
-      <ul className="list-links">
-        <li className="link">
-          <a href="#">Home</a>
-        </li>
-        <li className="link">
-          <a href="#">AboutUS</a>
-        </li>
-        <li className="link">
-          <a href="#">ContactUS</a>
-        </li>
-      </ul>
-      <div className="nav-btns">
-        <a href="#" className="btn-link">
-          Cart +
-        </a>
+    <div className="nav-bar">
+      <div className="nav">
+        <div className="logo-container">
+          <img src={require("../assest/logo.png")} alt="#" />
+        </div>
+        <ul className="list-links">
+          <li className="link">
+            <a href="#">Home</a>
+          </li>
+          <li className="link">
+            <a href="#">AboutUS</a>
+          </li>
+          <li className="link">
+            <a href="#">ContactUS</a>
+          </li>
+        </ul>
+        <div className="nav-btns">
+          <a href="#" className="btn-link">
+            Cart +
+          </a>
+        </div>
       </div>
     </div>
   );
